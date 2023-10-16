c4c5cc1f4c3c710dd517379e512bb94404292ac4
Euclid Stellar <euclidstellar@gmail.com>
2023-10-17 00:12:53 +0530

    commit for backup 1

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/dummy_question.dart';
+//import 'package:quiz_app/models/quiz_questions.dart';
 
 class Questions extends StatefulWidget {
-  const Questions({super.key});
+  const Questions({Key? key}) : super(key: key);
 
   @override
-  State<Questions> createState() {
+
+  _QuestionsState createState() {
     return _QuestionsState();
   }
 }
 
 class _QuestionsState extends State<Questions> {
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
     return Scaffold(
-      
       body: Center(
-        
         child: DecoratedBox(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
@@ -30,68 +32,46 @@ class _QuestionsState extends State<Questions> {
             ),
           ),
           child: SizedBox(
-            
-              width: double.infinity,
-              child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-             const Text('Question 1' , style: TextStyle(fontSize: 24 , color: Colors.white),),
-             const SizedBox(height: 20),
-             Answer(answertext: 'Answer 1' , onTap: (){},),
-             Answer(answertext: 'Answer 1' , onTap: (){},),
-             Answer(answertext: 'Answer 1' , onTap: (){},),
-             Answer(answertext: 'Answer 1' , onTap: (){},),
-          ],
-            
-              ),
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(currentQuestion.text,  style: const TextStyle(fontSize: 24, color: Colors.white)),
+                const SizedBox(height: 20),
+                Answer(answertext: currentQuestion.answers[0], onTap: () {}),
+                Answer(answertext: currentQuestion.answers[1], onTap: () {}),
+                Answer(answertext: currentQuestion.answers[2], onTap: () {}),
+                Answer(answertext: currentQuestion.answers[3], onTap: () {}),
+              ],
             ),
-        )
-      )
-      );
+          ),
+        ),
+      ),
+    );
   }
 }
 
-
-//     return  SizedBox(
-//       width: double.infinity,
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//            const Text('Question 1' , style: TextStyle(fontSize: 24 , color: Colors.white),),
-//            const SizedBox(height: 20),
-//            Answer(answertext: 'Answer 1' , onTap: (){},),
-//            Answer(answertext: 'Answer 1' , onTap: (){},),
-//            Answer(answertext: 'Answer 1' , onTap: (){},),
-//            Answer(answertext: 'Answer 1' , onTap: (){},),
-//         ],
-    
-//       ),
-//     );
-//   }
-// }
-
 class Answer extends StatelessWidget {
   const Answer({
-    super.key, required this.answertext, required this.onTap,
-  });
+    Key? key,
+    required this.answertext,
+    required this.onTap,
+  }) : super(key: key);
 
-  final String answertext ;
-  final void Function() onTap ;
+  final String answertext;
+  final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onTap,
       style: ElevatedButton.styleFrom(
-
-         
         foregroundColor: Colors.white, backgroundColor: Color.fromARGB(255, 1, 21, 16),
         padding: const EdgeInsets.symmetric(
-          horizontal: 20,
+          horizontal: 70,
           vertical: 12,
         ),
         shape: const StadiumBorder(),
-        
       ),
       child: Text(answertext),
     );
