f7bc809a012ec555bc6ef38011c029840ef88ab6
Euclid Stellar <euclidstellar@gmail.com>
2023-10-16 22:06:53 +0530

    main.dart restructured

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/screeen1.dart';
+//import 'package:quiz_app/game_screen.dart';
+import 'package:quiz_app/quiz.dart';
+//import 'package:quiz_app/screeen1.dart';
 
 void main() {
   runApp(const QuizApp());
-}
-
-class QuizApp extends StatefulWidget {
-const QuizApp({super.key});
-
-  @override
-  State<QuizApp> createState() => _QuizAppState();
-}
-
-class _QuizAppState extends State<QuizApp> {
-@override
-
-  Widget build(BuildContext context) {
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-
-              Colors.blue, 
-              Color.fromARGB(255, 116, 185, 180),
-              Color.fromARGB(255, 49, 196, 209),
-              Color.fromARGB(255, 103, 186, 106) ,
-
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const SplashScreen(),
-        ),
-      ),
-    );
-    
-  }
 }
